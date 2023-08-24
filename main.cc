b129c550e292575fcf103bf2664f4325567b7d2f
Constantine Khrulev <ckhroulev@alaska.edu>
2023-08-24 13:24:36 -0800

    Some modularization...

--- main.cc
+++ main.cc
@@ -11,70 +11,94 @@ extern "C" {
 #include "yac_interface.h"
 }
 
-int define_input_field(int instance_id, const char *comp_name, int comp_id,
-                       const char *field_name) {
+/*!
+ * Define a curvilinear Cartesian 2D grid on a sphere.
+ *
+ * `grid_x` and `grid_y` should define cell vertices (cell bounds)
+ *
+ * `point_x` and `point_y` correspond to cell centers (for grids used
+ * for conservative interpolation). Set to `nullptr` to define a grid
+ * using cell vertices as the point set.
+ */
+static int define_grid(const char *grid_name, int n_vertices[2], double *grid_x,
+                       double *grid_y, int n_points[2], double *point_x,
+                       double *point_y) {
 
   int cyclic[] = {0, 0};
+  int grid_id = 0;
+
+  yac_cdef_grid_curve2d(grid_name, n_vertices, cyclic, grid_x, grid_y,
+                        &grid_id);
+
+  int point_id = 0;
+  if (n_points != nullptr and point_x != nullptr and point_y != nullptr) {
+    yac_cdef_points_curve2d(grid_id, n_points, YAC_LOCATION_CELL, point_x,
+                            point_y, &point_id);
+  } else {
+    yac_cdef_points_curve2d(grid_id, n_vertices, YAC_LOCATION_CORNER, grid_x,
+                            grid_y, &point_id);
+  }
+
+  return point_id;
+}
+
+static int define_field(int comp_id, int point_id, const char *field_name) {
+
+  const char *time_step_length = "1";
+  const int point_set_size = 1;
+  const int collection_size = 1;
+
+  int field_id = 0;
+  yac_cdef_field(field_name, comp_id, &point_id, point_set_size,
+                 collection_size, time_step_length, YAC_TIME_UNIT_SECOND,
+                 &field_id);
+
+  return field_id;
+}
+
+int define_input_field(const char *comp_name, int comp_id,
+                       const char *grid_name, const char *field_name) {
+
   int n_vertices[] = {3, 3};
   double grid_x[] = {-1, 0, 1, -1, 0, 1, -1, 0, 1};
   double grid_y[] = {-1, -1, -1, 0, 0, 0, 1, 1, 1};
-  int grid_id = 0;
 
-  const int n_cells = 4;
   int n_points[] = {2, 2};
-  double points_x[n_cells] = {-0.5, 0.5, -0.5, 0.5};
-  double points_y[n_cells] = {-0.5, -0.5, 0.5, 0.5};
+  double points_x[] = {-0.5, 0.5, -0.5, 0.5};
+  double points_y[] = {-0.5, -0.5, 0.5, 0.5};
 
-  yac_cdef_grid_curve2d(comp_name, n_vertices, cyclic, grid_x, grid_y,
-                        &grid_id);
+  int point_id = define_grid(grid_name, n_vertices, grid_x, grid_y, n_points,
+                             points_x, points_y);
 
-  int point_id = 0;
-  yac_cdef_points_curve2d(grid_id, n_points, YAC_LOCATION_CELL, points_x,
-                          points_y, &point_id);
-
-  int point_set_size = 1;
-  int collection_size = 1;
-
-  int target_field_id = 0;
-  const char *time_step_length = "1";
-  yac_cdef_field(field_name, comp_id, &point_id, point_set_size,
-                 collection_size, time_step_length, YAC_TIME_UNIT_SECOND,
-                 &target_field_id);
-
-  return target_field_id;
+  return define_field(comp_id, point_id, field_name);
 }
 
-int define_target_field(int instance_id, const char *comp_name, int comp_id,
-                        const char *field_name) {
+int define_target_field(const char *comp_name, int comp_id,
+                        const char *grid_name, const char *field_name) {
 
-  int cyclic[] = {0, 0};
   int n_vertices[] = {2, 2};
-  double grid_x[] = {-0.5, 0.5, -0.5, 0.5};
-  double grid_y[] = {-0.5, -0.5, 0.5, 0.5};
-  int grid_id = 0;
+  double grid_x[] = {-0.25, 0.25, -0.25, 0.25};
+  double grid_y[] = {-0.25, -0.25, 0.25, 0.25};
 
-  const int n_cells = 1;
-  int n_points[] = {1, 1};
-  double points_x[n_cells] = {0};
-  double points_y[n_cells] = {0};
+  int point_id = define_grid(grid_name, n_vertices, grid_x, grid_y, nullptr,
+                             nullptr, nullptr);
 
-  yac_cdef_grid_curve2d(comp_name, n_vertices, cyclic, grid_x, grid_y,
-                        &grid_id);
+  return define_field(comp_id, point_id, field_name);
+}
 
-  int point_id = 0;
-  yac_cdef_points_curve2d(grid_id, n_points, YAC_LOCATION_CELL, points_x,
-                          points_y, &point_id);
+static int define_average_interpolation(double missing_value) {
+  int id = 0;
+  yac_cget_interp_stack_config(&id);
+  // add average
+  yac_cadd_interp_stack_config_average(id, YAC_AVG_DIST,
+                                       0 /* partial_coverage = false */);
+  // add nearest neighbor
+  yac_cadd_interp_stack_config_nnn(
+      id, YAC_NNN_DIST, 1 /* one nearest neighbor */, 1.0 /* scaling */);
+  // constant if the above failed
+  yac_cadd_interp_stack_config_fixed(id, missing_value);
 
-  int point_set_size = 1;
-  int collection_size = 1;
-
-  int target_field_id = 0;
-  const char *time_step_length = "1";
-  yac_cdef_field(field_name, comp_id, &point_id, point_set_size,
-                 collection_size, time_step_length, YAC_TIME_UNIT_SECOND,
-                 &target_field_id);
-
-  return target_field_id;
+  return id;
 }
 
 int main(int argc, char **argv) {
@@ -108,43 +132,30 @@ int main(int argc, char **argv) {
 
       // Define components: this has to be done using *one* call
       // (cannot call yac_cdef_comp?_instance() more than once)
-      const char *comp_names[2] = {"input", "output"};
-      int comp_ids[2] = {0, 0};
-      yac_cdef_comps_instance(instance_id, comp_names, 2, comp_ids);
-      int input_id = comp_ids[0];
-      int output_id = comp_ids[1];
+      const char *comp_name = "interpolation";
+      int comp_id = 0;
+      yac_cdef_comp_instance(instance_id, comp_name, &comp_id);
 
       // Define fields:
       int input_field =
-          define_input_field(instance_id, "input", input_id, "source");
+          define_input_field(comp_name, comp_id, "source", "source");
       int target_field =
-          define_target_field(instance_id, "output", output_id, "target");
+          define_target_field(comp_name, comp_id, "target", "target");
 
       // Define the interpolation stack:
-      int interp_stack_id = 0;
-      {
-        yac_cget_interp_stack_config(&interp_stack_id);
-        // add average
-        yac_cadd_interp_stack_config_average(interp_stack_id, YAC_AVG_DIST,
-                                             0 /* partial_coverage = false */);
-        yac_cadd_interp_stack_config_nnn(interp_stack_id, YAC_NNN_AVG,
-                                         1 /* one nearest neighbor */,
-                                         1.0 /* scaling */);
-        // constant if averaging failed
-        yac_cadd_interp_stack_config_fixed(interp_stack_id, -9999);
-      }
+      int interp_stack_id = define_average_interpolation(-9999.0);
 
       // Define the coupling between fields:
-      int src_lag = 0;
-      int tgt_lag = 0;
-      int mapping_side = 1;
+      const int src_lag = 0;
+      const int tgt_lag = 0;
+      const int mapping_side = 1; // 1 means "mapping on source"
       yac_cdef_couple_instance(
           instance_id,
-          "input",              // input component name
-          "input",              // input grid name
+          "interpolation",      // input component name
+          "source",             // input grid name
           "source",             // input field name
-          "output",             // target component name
-          "output",             // target grid name
+          "interpolation",      // target component name
+          "target",             // target grid name
           "target",             // target field name
           "1",                  // time step length in units below
           YAC_TIME_UNIT_SECOND, // time step length units
@@ -154,6 +165,9 @@ int main(int argc, char **argv) {
           tgt_lag,
           nullptr,              // weight file name (disabled)
           mapping_side);
+
+      // free the interpolation stack config now that we defined the coupling
+      yac_cfree_interp_stack_config(interp_stack_id);
 
       yac_cenddef_instance(instance_id);
 
@@ -168,12 +182,15 @@ int main(int argc, char **argv) {
       }
 
       {
-        double recv_field_data[1] = {-1.0};
+        const int N = 4;
+        double recv_field_data[N] = {-1.0, -1.0, -1.0, -1.0};
         double *recv_field[1] = {&recv_field_data[0]};
         int info;
         yac_cget(target_field, collection_size, recv_field, &info, &ierror);
 
-        log->message(2, "Got %f\n", recv_field_data[0]);
+        for (int k = 0; k < N; ++k) {
+          log->message(2, "Got %f\n", recv_field_data[k]);
+        }
       }
 
       yac_ccleanup_instance(instance_id);
