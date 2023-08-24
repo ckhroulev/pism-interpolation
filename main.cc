8505c2674a059b1ac6d720bba9c096a926766430
Constantine Khrulev <ckhroulev@alaska.edu>
2023-08-23 16:42:28 -0800

    It is doing something!

--- main.cc
+++ main.cc
@@ -11,9 +11,7 @@ extern "C" {
 #include "yac_interface.h"
 }
 
-
-
-int create_field(int instance_id, const char* comp_name, int comp_id, const char *field_name) {
+int define_field(int instance_id, const char* comp_name, int comp_id, const char *field_name) {
 
   int cyclic[] = {0, 0};
   int n_vertices[] = {2, 2};
@@ -32,7 +30,7 @@ int create_field(int instance_id, const char* comp_name, int comp_id, const char
   yac_cdef_points_curve2d(grid_id, n_points, YAC_LOCATION_CELL, points_x,
                           points_y, &point_id);
 
-  int point_set_size = 1;
+  int point_set_size = n_cells;
   int collection_size = 1;
 
   int target_field_id = 0;
@@ -63,58 +61,65 @@ int main(int argc, char **argv) {
     log->message(2, "Reading from %s...\n", input->c_str());
 
     {
+      // Initialize an instance:
       int instance_id = 0;
-      yac_cinit_instance(&instance_id);
-      yac_cdef_calendar(YAC_PROLEPTIC_GREGORIAN);
-      // Note: zero-padding of months and days *is* required.
-      yac_cdef_datetime_instance(instance_id, "-1-01-01", "+1-01-01");
+      {
+        yac_cinit_instance(&instance_id);
+        yac_cdef_calendar(YAC_PROLEPTIC_GREGORIAN);
+        // Note: zero-padding of months and days *is* required.
+        yac_cdef_datetime_instance(instance_id, "-1-01-01", "+1-01-01");
+      }
 
-      int interp_stack_id = 0;
-      yac_cget_interp_stack_config(&interp_stack_id);
-      // add average
-      yac_cadd_interp_stack_config_average(interp_stack_id, YAC_AVG_DIST, 0);
-      // constant if averaging failed
-      yac_cadd_interp_stack_config_fixed(interp_stack_id, -9999);
-
-      int src_lag = 0;
-      int tgt_lag = 0;
-      int mapping_side = 1;
-      yac_cdef_couple_instance(
-          instance_id,
-          "input", "input", "source",
-          "output", "output", "target",
-          "1", YAC_TIME_UNIT_SECOND, YAC_REDUCTION_TIME_NONE,
-          interp_stack_id, src_lag, tgt_lag, nullptr, mapping_side);
-
-      /*
-        Setup:
-
-        1. Initialize an instance
-        2. Define a component.
-        3. Define a grid.
-        4. Define points on the grid.
-        5. Define a field using these points.
-
-        Once this is done, use yac_cput() to send and yac_cget() to
-        receive data.
-        */
-
-      // Create components: this has to be done using *one* call:
+      // Define components: this has to be done using *one* call
+      // (cannot call yac_cdef_comp?_instance() more than once)
       const char *comp_names[2] = {"input", "output"};
       int comp_ids[2] = {0, 0};
       yac_cdef_comps_instance(instance_id, comp_names, 2, comp_ids);
       int input_id = comp_ids[0];
       int output_id = comp_ids[1];
 
-      int input_field = create_field(instance_id, "input", input_id, "source");
-      int target_field = create_field(instance_id, "output", output_id, "target");
+      // Define fields:
+      int input_field = define_field(instance_id, "input", input_id, "source");
+      int target_field = define_field(instance_id, "output", output_id, "target");
+
+      // Define the interpolation stack:
+      int interp_stack_id = 0;
+      {
+        yac_cget_interp_stack_config(&interp_stack_id);
+        // add average
+        yac_cadd_interp_stack_config_average(interp_stack_id, YAC_AVG_DIST, 0);
+        yac_cadd_interp_stack_config_nnn(interp_stack_id, YAC_NNN_AVG, 1, 1.0);
+        // constant if averaging failed
+        yac_cadd_interp_stack_config_fixed(interp_stack_id, -9999);
+      }
+
+      // Define the coupling between fields:
+      int src_lag = 0;
+      int tgt_lag = 0;
+      int mapping_side = 1;
+      yac_cdef_couple_instance(
+          instance_id,
+          "input",              // input component name
+          "input",              // input grid name
+          "source",             // input field name
+          "output",             // target component name
+          "output",             // target grid name
+          "target",             // target field name
+          "1",                  // time step length in units below
+          YAC_TIME_UNIT_SECOND, // time step length units
+          YAC_REDUCTION_TIME_NONE, // reduction in time (for asynchronous coupling)
+          interp_stack_id,
+          src_lag,
+          tgt_lag,
+          nullptr,              // weight file name (disabled)
+          mapping_side);
 
       yac_cenddef_instance(instance_id);
 
       int collection_size = 1;
       int ierror = 0;
       {
-        double send_field_data[2] = {3.0, 4.0};
+        double send_field_data[1] = {3.0};
         double *send_field_[1] = {&send_field_data[0]};
         double **send_field[1] = {&send_field_[0]};
         int info;
@@ -122,10 +127,12 @@ int main(int argc, char **argv) {
       }
 
       {
-        double recv_field_data[2] = {-1.0, -1.0};
+        double recv_field_data[1] = {-1.0};
         double *recv_field[1] = {&recv_field_data[0]};
         int info;
         yac_cget(target_field, collection_size, recv_field, &info, &ierror);
+
+        log->message(2, "Got %f\n", recv_field_data[0]);
       }
 
       yac_ccleanup_instance(instance_id);
