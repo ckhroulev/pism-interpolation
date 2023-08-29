64920d2d47ce01dfe939a2cfaf91a078335b21c7
Constantine Khrulev <ckhroulev@alaska.edu>
2023-08-29 12:22:23 -0800

    Link to NetCDF (YAC requires it now)

--- configure.sh
+++ configure.sh
@@ -6,6 +6,7 @@ set -e
 
 YAC=$HOME/local/dkrz
 PISM=$HOME/local/pism
+NETCDF=$HOME/local/netcdf
 
 prefix=$HOME/local/pism-interp
 
@@ -13,7 +14,7 @@ cmake -B build -S . \
       -DCMAKE_COLOR_MAKEFILE=OFF \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
       -DCMAKE_INSTALL_PREFIX=${prefix} \
-      -DCMAKE_PREFIX_PATH="${YAC};${PISM}"
+      -DCMAKE_PREFIX_PATH="${YAC};${PISM};${NETCDF}"
 
 rm -f compile_commands.json
 ln -s build/compile_commands.json .
