bdf079450bfbe2e2167e18779869a844d02cd62c
Constantine Khrulev <ckhroulev@alaska.edu>
2024-02-27 10:37:45 -0900

    I don't think we need to specify the NetCDF location

--- configure.sh
+++ configure.sh
@@ -6,7 +6,6 @@ set -e
 
 YAC=$HOME/local/dkrz
 PISM=$HOME/local/pism
-NETCDF=$HOME/local/netcdf
 
 prefix=$HOME/local/pism-interp
 
@@ -15,7 +14,7 @@ cmake -B build -S . \
       -DCMAKE_COLOR_MAKEFILE=OFF \
       -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
       -DCMAKE_INSTALL_PREFIX=${prefix} \
-      -DCMAKE_PREFIX_PATH="${YAC};${PISM};${NETCDF}"
+      -DCMAKE_PREFIX_PATH="${YAC};${PISM}"
 
 rm -f compile_commands.json
 ln -s build/compile_commands.json .
